9b2f8be9f236bf2ed31731075ecc7dd469a2a99f
devraphy <devraphy@gmail.com>
2021-05-17 23:55:31 +0900

    learned about computed property

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,35 @@
 <template>
-  <h1
-    @[event]="add"
-    :[attr]="'active'">
-    {{ msg }}
-  </h1>
+  <h1>{{ computedMessage }}</h1>
+  <h1>{{ computedMessage }}</h1>
+  <h1>{{ computedMessage }}</h1>
+  <h1>{{ computedMessage }}</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      msg: 'active',
-      attr: 'class',
-      event: 'click'
+      msg: "Hello Computed!"
+    }
+  },
+  computed: { 
+    // computed는 연산된 data를 관리하는 공간이다. 
+    // 아래의 computedMessage()는 메소드처럼 선언되었지만
+    // data처럼 명시 및 사용됨 기억하자. 
+    computedMessage() {
+      return this.msg.split('').reverse().join('')
     }
   },
   methods: {
-    add() {
-      this.msg += '!'
+    reverseMessage() {
+      return this.msg.split('').reverse().join('')
     }
-  }  
+  }
 }
 </script>
 
-<style lang="scss">
-
-  .active {
-    color: royalblue;
-    font-size: 100px;
-  }
-
+<style>
+ h1{
+   font-size: 50px;
+ }
 </style>
