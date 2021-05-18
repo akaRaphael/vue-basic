6156c35eaa4995159747f86c948c8b9d9e7ddadd
devraphy <devraphy@gmail.com>
2021-05-18 19:41:58 +0900

    learned about getter, setter, watch property, and class/style binding

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,32 @@
 <template>
-  <h1>{{ computedMessage }}</h1>
-  <h1>{{ computedMessage }}</h1>
-  <h1>{{ computedMessage }}</h1>
-  <h1>{{ computedMessage }}</h1>
+  <!-- key와 value가 같으면 key만 적어도 된다. 
+    ex) color: color  ==> color -->
+
+  <h1
+    :style="[fontStyle, backgroundStyle]"
+    @click="changeStyle">
+    Hello?!
+  </h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      msg: "Hello Computed!"
-    }
-  },
-  computed: { 
-    // computed는 연산된 data를 관리하는 공간이다. 
-    // 아래의 computedMessage()는 메소드처럼 선언되었지만
-    // data처럼 명시 및 사용됨 기억하자. 
-    computedMessage() {
-      return this.msg.split('').reverse().join('')
+      fontStyle: {
+        color: 'orange',
+        fontSize: '30px'
+      },
+      backgroundStyle: {
+        backgroundColor: 'gray'
+      }
     }
   },
   methods: {
-    reverseMessage() {
-      return this.msg.split('').reverse().join('')
+    changeStyle() {
+      this.fontStyle.color = 'royalblue'
+      this.fontStyle.fontSize = '100px'
     }
   }
 }
 </script>
-
-<style>
- h1{
-   font-size: 50px;
- }
-</style>
