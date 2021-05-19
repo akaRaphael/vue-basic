feeae7aabe8092b9cab2f8e63daceb0295f4430b
devraphy <devraphy@gmail.com>
2021-05-19 20:16:46 +0900

    learned about vue event handler

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,18 @@
 <template>
-  <button @click="handler">
+  <button @click="handlerA(), handlerB()">
     Click me!!!
   </button>
-  <ul>
-    <li
-      v-for="{id, name} in newFruits"
-      :key="id">
-      {{ id }} = {{ name }}
-    </li>
-  </ul>
 </template>
 
 <script>
-import shortid from 'shortid'
 export default {
-  data() {
-    return {
-      fruits: ['Apple', 'Banana', 'Cherry'],
-    }
-  },
-  computed: {
-    newFruits() {
-      return this.fruits.map(fruit => ({
-        id: shortid.generate(), // generate() => shortid를 생성해주는 메소드
-        name: fruit
-      }))
-    }
-  },
   methods: {
-    handler() {
-      this.fruits.push('Orange')
-    }
+    handlerA() {
+      console.log('handler A!')
+    },
+    handlerB() {
+      console.log('handler B!')
+    },
   }
 }
 </script>
-
-<style>
-  button, li {
-    font-size: 50px;
-  }
-</style>
