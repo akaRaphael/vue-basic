794d3d95aee6337ca0be3203b7e2d669e7034059
devraphy <devraphy@gmail.com>
2021-05-21 00:56:55 +0900

    learned about v-model, props, and slot

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <input type="text" />
+  <MyBtn>Banana</MyBtn>
+  <MyBtn 
+    :color="color">
+    Apple
+  </MyBtn>
+  <MyBtn 
+    large
+    color="salmon">
+    Cherry
+  </MyBtn>
+  <MyBtn><span style="color: orange;">Mango</span></MyBtn>
 </template>
 
 <script>
+import MyBtn from '~/components/MyBtn'
 export default {
+  components: {
+    MyBtn
+  },
   data() {
     return {
-      
+      color: 'royalblue'
     }
   }
 }
-</script>
+</script> 
